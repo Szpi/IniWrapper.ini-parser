4bece9736663f33d12e30cf376c81f909ebe095e
Tisu <piotrszkudlarski@gmail.com>
2019-01-17 21:46:24 +0100

    Integration IniWrapper with ini-parser library

---
+++ IniWrapper.ini-parser/IniWrapper.ini-parser/IniParserAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using IniParser;
+using IniParser.Model;
+using IniWrapper.ParserWrapper;
+using IniWrapper.Settings;
+
+namespace IniWrapper.ini_parser
+{
+    public class IniParserAdapter : IIniParser
+    {
+        private readonly IniSettings _iniSettings;
+        private readonly IniData _iniSaveData;
+
+        private IniData _iniReadData;
+        public IniParserAdapter(IniSettings iniSettings)
+        {
+            _iniSettings = iniSettings;
+            _iniSaveData = new IniData();
+        }
+
+        public string Read(string section, string key)
+        {
+            ReadFromFile();
+            return _iniReadData[section][key];
+        }
+
+        public void Write(string section, string key, string value)
+        {
+            _iniSaveData[section][key] = value;
+            var parser = new FileIniDataParser();
+            parser.WriteFile(_iniSettings.IniFilePath, _iniSaveData);
+        }
+
+        private void ReadFromFile()
+        {
+            if (_iniReadData != null)
+            {
+                return;
+            }
+
+            var parser = new FileIniDataParser();
+            _iniReadData = parser.ReadFile(_iniSettings.IniFilePath);
+        }
+    }
+}
---
+++ IniWrapper.ini-parser/IniWrapper.ini-parser/IniWrapperExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using IniWrapper.Settings;
+using IniWrapper.Wrapper;
+
+namespace IniWrapper.ini_parser
+{
+    public static class IniWrapperExtension
+    {
+        public static IIniWrapper CreateWithIniParser(this IIniWrapperFactory iniWrapperFactory, IniSettings iniSettings)
+        {
+            var parser = new IniParserAdapter(iniSettings);
+            return iniWrapperFactory.Create(iniSettings, parser);
+        }
+
+        public static IIniWrapper CreateWithIniParser(this IIniWrapperFactory iniWrapperFactory, Action<IniSettings> iniSettings)
+        {
+            var settings = new IniSettings();
+
+            iniSettings(settings);
+
+            return iniWrapperFactory.CreateWithIniParser(settings);
+        }
+
+        public static IIniWrapper CreateWithIniParser(this IniWrapperFactory iniWrapperFactory, IniSettings iniSettings)
+        {
+            var parser = new IniParserAdapter(iniSettings);
+            return iniWrapperFactory.Create(iniSettings, parser);
+        }
+
+        public static IIniWrapper CreateWithIniParser(this IniWrapperFactory iniWrapperFactory, Action<IniSettings> iniSettings)
+        {
+            var settings = new IniSettings();
+
+            iniSettings(settings);
+
+            return iniWrapperFactory.CreateWithIniParser(settings);
+        }
+    }
+}
